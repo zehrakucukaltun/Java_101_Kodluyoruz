57211e7c248d4451c87dad6d92d7fb7622530f51
zehrakucukaltun <76599330+zehrakucukaltun@users.noreply.github.com>
2021-08-12 17:22:55 +0300

    Add files via upload

---
+++ FindArea.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class FindArea {
+    public static void main(String[] args) {
+        Scanner input =new Scanner(System.in);
+        System.out.println("Enter the value a:");
+        int a = input.nextInt();
+        System.out.println("Enter the value b:");
+        int b = input.nextInt();
+        System.out.println("Enter value c:");
+        int c =input.nextInt();
+        double u = (a + b + c)/2 ;
+        double area = Math.sqrt(u * (u-a)* (u-b)* (u-c));
+        System.out.println("Area of triangle is:" + area);
+
+
+
+
+    }
+}
