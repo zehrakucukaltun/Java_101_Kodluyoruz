17a5b57230fce61ba2c15f97e5675de4f6758d5f
zehrakucukaltun <76599330+zehrakucukaltun@users.noreply.github.com>
2021-08-16 18:24:07 +0300

    Add files via upload

---
+++ FindLeapYear.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class FindLeapYear {
+    public static void main(String[] args) {
+        int year;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the year value :");
+        year = input.nextInt();
+        if (year % 4 == 0 ){
+            if(year % 100 == 0) {
+                if( year %400 ==0){
+                    System.out.println(year + " is a leap year.");
+                }else {
+                    System.out.println(year + " is not a leap year. ");
+                }
+            }else{
+                System.out.println(year + " is leap year.");
+            }
+        } else {
+            System.out.println(year + " is not a leap year.");
+            }
+        }
+    }
+
